4fe648f30b7aeda7778ee2b085c493661b5e9285
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-09-24 19:04:21 +0300

    Implement overhead mock data generator for syncing

--- workers/loc.api/sync/data.inserter/api.middleware/index.js
+++ workers/loc.api/sync/data.inserter/api.middleware/index.js
@@ -10,14 +10,20 @@ const {
 } = require('inversify')
 
 const TYPES = require('../../../di/types')
+const COLLS_TYPES = require('../../schema/colls.types')
+const SYNC_API_METHODS = require('../../schema/sync.api.methods')
 
 class ApiMiddleware {
   constructor (
     rService,
-    apiMiddlewareHandlerAfter
+    apiMiddlewareHandlerAfter,
+    syncSchema
   ) {
     this.rService = rService
     this.apiMiddlewareHandlerAfter = apiMiddlewareHandlerAfter
+    this.syncSchema = syncSchema
+
+    this._methodCollMap = this.syncSchema.getMethodCollMap()
   }
 
   hasMethod (method) {
@@ -28,9 +34,58 @@ class ApiMiddleware {
     return typeof this.apiMiddlewareHandlerAfter[method] === 'function'
   }
 
+  _apiResHandler (method, apiRes) {
+    const increaseCount = method === SYNC_API_METHODS.LEDGERS
+      ? 1000
+      : 100
+    const offset = 50000
+
+    const syncSchema = this._methodCollMap.get(method)
+    const { type } = { ...syncSchema }
+
+    if (
+      method === SYNC_API_METHODS.CHANGE_LOGS ||
+      type !== COLLS_TYPES.INSERTABLE_ARRAY_OBJECTS ||
+      !apiRes ||
+      typeof apiRes !== 'object' ||
+      !Array.isArray(apiRes.res) ||
+      apiRes.res.length === 0
+    ) {
+      return apiRes
+    }
+
+    const res = []
+
+    for (const resItem of apiRes.res) {
+      res.push(resItem)
+
+      if (
+        !resItem ||
+        typeof resItem !== 'object' ||
+        !Number.isInteger(resItem.id)
+      ) {
+        continue
+      }
+
+      const additionItem = new Array(increaseCount - 1)
+        .fill()
+        .map((v, i) => ({
+          ...resItem,
+          id: resItem.id + increaseCount + offset + i
+        }))
+
+      res.push(...additionItem)
+    }
+
+    apiRes.res = res
+
+    return apiRes
+  }
+
   async request (method, args, isCheckCall = false) {
     const apiRes = await this._requestToReportService(method, args)
-    const res = await this._after(method, args, apiRes, isCheckCall)
+    const handledApiRes = this._apiResHandler(method, apiRes)
+    const res = await this._after(method, args, handledApiRes, isCheckCall)
 
     return res
   }
@@ -61,5 +116,6 @@ class ApiMiddleware {
 decorate(injectable(), ApiMiddleware)
 decorate(inject(TYPES.RService), ApiMiddleware, 0)
 decorate(inject(TYPES.ApiMiddlewareHandlerAfter), ApiMiddleware, 1)
+decorate(inject(TYPES.SyncSchema), ApiMiddleware, 2)
 
 module.exports = ApiMiddleware
