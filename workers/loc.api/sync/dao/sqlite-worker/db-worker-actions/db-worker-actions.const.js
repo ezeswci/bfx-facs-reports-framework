344d78d95902941bfb7917afd48641783f04618c
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-10-08 17:58:12 +0300

    Add sqlite worker run-action

--- workers/loc.api/sync/dao/sqlite-worker/db-worker-actions/db-worker-actions.const.js
+++ workers/loc.api/sync/dao/sqlite-worker/db-worker-actions/db-worker-actions.const.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = {
+  RUN: 'RUN',
   RUN_IN_TRANS: 'RUN_IN_TRANS',
   EXEC_PRAGMA: 'EXEC_PRAGMA'
 }
--- workers/loc.api/sync/dao/sqlite-worker/db-worker-actions/index.js
+++ workers/loc.api/sync/dao/sqlite-worker/db-worker-actions/index.js
@@ -6,12 +6,16 @@ const dbWorkerActions = require(
 
 const DB_WORKER_ACTIONS = require('./db-worker-actions.const')
 
+const actionRun = require('./action-run')
 const actionRunInTrans = require('./action-run-in-trans')
 const actionExecPragma = require('./action-exec-pragma')
 
 module.exports = (db, args) => {
   const { action, sql, params } = args
 
+  if (action === DB_WORKER_ACTIONS.RUN) {
+    return actionRun(db, sql, params)
+  }
   if (action === DB_WORKER_ACTIONS.RUN_IN_TRANS) {
     return actionRunInTrans(db, sql, params)
   }
