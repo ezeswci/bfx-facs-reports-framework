bacd6250d7be345bdc5d2e867a8f8d766415c89e
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-11-12 14:30:16 +0200

    Remove sync mock data generation

--- workers/loc.api/sync/data.inserter/api.middleware/index.js
+++ workers/loc.api/sync/data.inserter/api.middleware/index.js
@@ -10,8 +10,6 @@ const {
 } = require('inversify')
 
 const TYPES = require('../../../di/types')
-const COLLS_TYPES = require('../../schema/colls.types')
-const SYNC_API_METHODS = require('../../schema/sync.api.methods')
 
 class ApiMiddleware {
   constructor (
@@ -34,68 +32,9 @@ class ApiMiddleware {
     return typeof this.apiMiddlewareHandlerAfter[method] === 'function'
   }
 
-  _logTime (method) {
-    if (method) {
-      console.log('[METHOD]:'.bgBlue, method)
-    }
-
-    console.log('[TIME]:'.bgRed, new Date().toUTCString())
-  }
-
-  _apiResHandler (method, apiRes) {
-    this._logTime(method)
-
-    const increaseCount = method === SYNC_API_METHODS.LEDGERS
-      ? 2000
-      : 300
-    const offset = 50000
-
-    const syncSchema = this._methodCollMap.get(method)
-    const { type } = { ...syncSchema }
-
-    if (
-      method === SYNC_API_METHODS.CHANGE_LOGS ||
-      type !== COLLS_TYPES.INSERTABLE_ARRAY_OBJECTS ||
-      !apiRes ||
-      typeof apiRes !== 'object' ||
-      !Array.isArray(apiRes.res) ||
-      apiRes.res.length === 0
-    ) {
-      return apiRes
-    }
-
-    const res = []
-
-    for (const resItem of apiRes.res) {
-      res.push(resItem)
-
-      if (
-        !resItem ||
-        typeof resItem !== 'object' ||
-        !Number.isInteger(resItem.id)
-      ) {
-        continue
-      }
-
-      const additionItem = new Array(increaseCount - 1)
-        .fill()
-        .map((v, i) => ({
-          ...resItem,
-          id: resItem.id + increaseCount + offset + i
-        }))
-
-      res.push(...additionItem)
-    }
-
-    apiRes.res = res
-
-    return apiRes
-  }
-
   async request (method, args, isCheckCall = false) {
     const apiRes = await this._requestToReportService(method, args)
-    const handledApiRes = this._apiResHandler(method, apiRes)
-    const res = await this._after(method, args, handledApiRes, isCheckCall)
+    const res = await this._after(method, args, apiRes, isCheckCall)
 
     return res
   }
